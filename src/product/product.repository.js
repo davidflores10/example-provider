38a9ab91e109423d46023373291d5f22f5bb068f
dfloresleroy <david.flores@ext.leroymerlin.es>
2024-11-08 12:36:06 +0100

    fix integer type

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,7 +4,7 @@ class ProductRepository {
   constructor() {
     this.products = new Map([
       ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "29", "v1")],
+      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", 29, "v1")],
       ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
     ]);
   }

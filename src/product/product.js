0f3295d7bf00aba868e000dddb23410b0cc8347a
dfloresleroy <david.flores@ext.leroymerlin.es>
2024-11-08 09:29:51 +0100

    test update Pact adding year

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,11 @@
 class Product {
-    constructor(id, type, name, version) {
-        this.id = id;
-        this.type = type;
-        this.name = name;
-        this.version = version;
-    }
+  constructor(id, type, name, year, version) {
+    this.id = id;
+    this.type = type;
+    this.name = name;
+    this.yerar = year;
+    this.version = version;
+  }
 }
 
 module.exports = Product;
